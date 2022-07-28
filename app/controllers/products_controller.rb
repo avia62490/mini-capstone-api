1b23b5b7a03f91a1a400fd3fdc263470cfe1bd0a
Alex Via <alexandervia@gmail.com>
2022-07-28 12:52:07 -0500

    Adds Create route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,17 @@ def show
   render json: Product.find_by(id: params[:id])
 end
 
+def create
+  product = Product.new(
+  name: params[:name],
+  price: params[:price],
+  image_url: params[:image_url],
+  description: params[:description]
+  )
+  product.save
+  render json: product.as_json
+end
+
+
 
 end
