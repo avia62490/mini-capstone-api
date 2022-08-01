b7df9388a12ce1c3266e6ddb49f8592e9a624f9a
Alex Via <alexandervia@gmail.com>
2022-08-01 15:02:34 -0500

    adds friendly_created_at, is_discounted?, tax, and total methods to Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,14 +4,16 @@ def index
   p "="*99
   p "All the Products"
   p "="*99
-  render json: Product.all
+  products = Product.all
+  render json: products.as_json
 end
 
 def show
   p "="*99
   p "A Specific Product"
   p "="*99
-  render json: Product.find_by(id: params[:id])
+  product = Product.find_by(id: params[:id])
+  render json: product.as_json(methods: [:friendly_created_at, :is_discounted?, :tax, :total])
 end
 
 def create
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if price <= 10
+      true
+    else
+      false
+    end
+  end
+  
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
+
+  def total
+    price + tax
+  end
 end
