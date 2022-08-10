86a1706c48efc799175f9693fa50d5961dfd6d2e
Alex Via <alexandervia@gmail.com>
2022-08-09 19:05:56 -0500

    Makes create order method dynamic for user_id, product_id, and quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,13 +10,13 @@ class OrdersController < ApplicationController
     p "=" * 99
     p "ORDER CREATED"
     p "=" * 99
-    if current_user
-      @order = Order.new(user_id: 2, product_id: 2, quantity: 1)
-      @order.save
-      # render json: {measd: "order saved"}
-      render template: "orders/show"
-    else
-      render json: {messGE: "Must log in"}
-    end
+    @order = Order.new(
+    user_id: current_user.id, 
+    product_id: params[:product_id], 
+    quantity: params[:quantity]
+    )
+    @order.save
+    # render json: @order.as_json
+    render template: "orders/show"
   end
 end
