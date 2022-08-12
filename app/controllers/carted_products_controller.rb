6c9022e154702a8a0e5e0716020653104d047b71
Alex Via <alexandervia@gmail.com>
2022-08-12 11:53:01 -0500

    Makes carted_products create route dynamic for product_id and quantity

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -3,8 +3,8 @@ class CartedProductsController < ApplicationController
   def create
     carted_product = CartedProduct.new(
       user_id: current_user.id,
-      product_id: 10,
-      quantity: 6,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
       status: "In cart"
     )
     carted_product.save
