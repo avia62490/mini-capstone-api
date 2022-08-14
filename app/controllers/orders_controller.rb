6670ef43bdb95282a92f4c146254a49f7e87e029
Alex Via <alexandervia@gmail.com>
2022-08-14 15:41:00 -0500

    Updates associations, order create method saves order in databse

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,14 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: params[:id])
-    # render template: "orders/show"
-    render json: @order.as_json
+    render template: "orders/show"
+    # render json: @order.as_json
   end
 
   def create
     carted_products = CartedProduct.where(user_id: current_user.id, status: "In cart")
+
+    #Get Price values
     order_subtotal = 0
     carted_products.each do |cp|
       cost_of_product = cp.product.price * cp.quantity
@@ -16,12 +18,15 @@ class OrdersController < ApplicationController
     end
     order_tax = order_subtotal * 0.09
     order_total = order_subtotal + order_tax
+
+    #Create order
     order = Order.new(
     user_id: current_user.id,
     sub_total: order_subtotal,
     tax: order_tax,
     total: order_total
     )
+    order.save!
     render json: order.as_json
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   has_many :images
   has_many :category_products
   has_many :categories, through: :category_products
-  has_many :orders
   has_many :carted_products
 
   validates :name, presence: true
