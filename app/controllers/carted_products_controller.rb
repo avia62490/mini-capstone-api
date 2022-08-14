5198e7cef01017d737a89a3037f5b485a930245b
Alex Via <alexandervia@gmail.com>
2022-08-14 16:59:03 -0500

    Adds carted_product destroy method - removes carted_product from cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -13,7 +13,15 @@ class CartedProductsController < ApplicationController
   end
 
   def index
-    carted_products = CartedProduct.where(user_id: current_user.id, status: "In cart")
-    render json: carted_products.as_json
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: "In cart")
+    render template: "carted_products/index"
+    # render json: carted_products.as_json
+  end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.status = "Removed" 
+    carted_product.save
+    render json: {message: "Item removed from shopping cart"}
   end
 end
